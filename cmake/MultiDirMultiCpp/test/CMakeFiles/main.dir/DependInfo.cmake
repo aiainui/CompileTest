e377a7de4f779d9dd733409245c4be56378921b4
aiainui <379879419@qq.com>
2018-12-28 02:32:03 +0800

    up

---
+++ cmake/MultiDirMultiCpp/test/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dengyulong/CompileTest/cmake/MultiDirMultiCpp/test/main.cpp" "/home/dengyulong/CompileTest/cmake/MultiDirMultiCpp/test/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/dengyulong/CompileTest/cmake/MultiDirMultiCpp/src/CMakeFiles/libDrawing.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake/MultiDirMultiCpp/test/main.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: maim.cpp
+	> Author: dyl
+	> Mail: denyulong@100tal.com
+	> Created Time: Sun 24 Dec 2018 06:51:01 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include "../include/Drawing.h"
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "I am drawing a heart..." << endl;
+    Draw();
+    proc();
+    return 0;
+}
